5f6b4a557b24f186d923d7b7be8484cf7ec3af74
WAPEETY <wapeety@protonmail.com>
2020-10-20 16:20:52 +0200

    added driving mode, added not conntected, NOT TESTED

--- Arduino Project/E-Bike-LED-System/E-Bike-LED-System.ino
+++ Arduino Project/E-Bike-LED-System/E-Bike-LED-System.ino
@@ -1,7 +1,9 @@
 #include "BluetoothSerial.h"
 #include "FastLED.h"
+#include "Wire.h"
+#include "MPU6050_tockn.h"
 
-#define NUM_LEDS 60
+#define NUM_LEDS 17
 #define PIN 2
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -10,30 +12,27 @@
 
 BluetoothSerial BT;
 
+MPU6050 mpu(Wire);
+
 CRGB leds[60];
 
 TaskHandle_t BTfunction;
 TaskHandle_t LEDfunction;
+TaskHandle_t Gyroscope;
 
-String modeS;
-String redS;
-String greenS;
-String blueS;
-String brightnessS;
+String modeS, redS, greenS, blueS, brightnessS;
 
-int counter;
-int mode;
-int red;
-int green;
-int blue;
-int brightness;
+int counter, mode;
+int red, green, blue, brightness;
 
-bool done;
+bool done, connection = false;
+
+float temp, accY, accX, accZ, acceleration, prevAcceleration;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial Started, waiting for cores");
-  FastLED.addLeds<NEOPIXEL, PIN>(leds, 60);
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, 17);
   
   xTaskCreatePinnedToCore(
                     BTserver,
@@ -42,6 +41,16 @@ void setup() {
                     NULL,
                     1,
                     &BTfunction,
+                    0);
+  delay(500);
+
+  xTaskCreatePinnedToCore(
+                    I2Cgyroscope,
+                    "Gyroscope",
+                    10000,
+                    NULL,
+                    1,
+                    &Gyroscope,
                     0);
   delay(500);
   
@@ -76,6 +85,9 @@ void BTserver(void * pvParameters){
     
     while(BT.available()) {
       character = BT.read();
+      if(character != '\0'){
+        connection = true;
+      }
       content.concat(character);
       newMessage = true;
     }
@@ -92,10 +104,10 @@ void BTserver(void * pvParameters){
       }
       if(counter == 3 and content[i]!= ','){
         blueS += content[i];
-      }/*nuovo*/
+      }
       if(counter == 4 and content[i]!= ','){
         brightnessS += content[i];
-      }/*/nuovo*/
+      }
       if(content[i] == ','){
         counter ++;
         done = true;
@@ -123,6 +135,22 @@ void BTserver(void * pvParameters){
   }
 }
 
+void I2Cgyroscope(void * pvParameters){
+  Serial.print("Gyroscope Data Function running on core ");
+  Serial.println(xPortGetCoreID());
+  Wire.begin(21,22);
+  mpu.begin();
+  mpu.calcGyroOffsets(true);
+  while(true){
+    mpu.update();
+    //Serial.println(mpu.getAccY());
+    //Serial.println(mpu.getTemp());
+    prevAcceleration = acceleration;
+    acceleration = mpu.getAccY();
+    delay(500);
+  }
+}
+
 void LEDmanager(void * pvParameters){
   Serial.print("LED Control Function running on core ");
   Serial.println(xPortGetCoreID());
@@ -131,12 +159,19 @@ void LEDmanager(void * pvParameters){
     
       while(mode == 0){
         Serial.println("Solid color");
-        
-        FastLED.setBrightness(brightness);
-        fill_solid( leds, NUM_LEDS, CRGB(red , green , blue));
-        FastLED.show();
-        delay(1000);
-        
+
+        if(connection == false){
+            FastLED.setBrightness(255);
+            fill_solid( leds, NUM_LEDS, CRGB(random(255) , random(255), random(255)));
+            FastLED.show();
+            delay(1000);
+        }
+        else{
+            FastLED.setBrightness(brightness);
+            fill_solid( leds, NUM_LEDS, CRGB(red , green , blue));
+            FastLED.show();
+            delay(1000);
+        }
       }
       while(mode == 1){
         Serial.println("Walking");
@@ -188,11 +223,16 @@ void LEDmanager(void * pvParameters){
       }
       while(mode == 4){
         Serial.println("Driving Mode");
-
-        FastLED.setBrightness(150);
-        fill_solid( leds, NUM_LEDS, CRGB(0 , 255 , 0));
-        FastLED.show();
-        /*this will be implemented in future, driving mode requires sensors*/
+        if(prevAcceleration > acceleration){
+            FastLED.setBrightness(150);
+            fill_solid( leds, NUM_LEDS, CRGB(0 , 255 , 0));
+            FastLED.show(); 
+          }
+        else{
+            FastLED.setBrightness(150);
+            fill_solid( leds, NUM_LEDS, CRGB(0 , 255 , 0));
+            FastLED.show();
+          }
         delay(25);
       }
     }
--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Anyway, in future I'll post here all the images for the electronic stuff, here i
 * A bunch of jumper wires
 * A breadboard
 
-In september I will transfer this to my bike using a piece of proofboard and I will write a proper description and documentation.
+~~In september I will transfer this to my bike using a piece of proofboard and I will write a proper description and documentation.~~ Yes... it's october and I didn't tested this ~~sh*t~~.
 
 **THE APP WAS MADE WITH KODULAR SO DON'T ASK ME FOR THE SOURCE CODE** just take the .aia file and import it in a project.
+
+**UPDATE 0.4.3**
+
+Since I changed Kodular account you have to uninstall the app and reinstall it otherwise you'll find two application on your phone.
